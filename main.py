47e5a56adb909a6629b29d5ff3a20e6fc3d8ea7e
Wladyslaw <opensayshe@protonmail.com>
2024-07-27 22:10:12 +0300

    v0.012

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# https://github.com/opensayshe/tg-dlp v0.012
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import yt_dlp
@@ -6,13 +7,16 @@ import concurrent.futures
 import threading
 import time
 
-# Semaphore to limit concurrent downloads
+# concurrent downloads limit
 download_semaphore = threading.Semaphore(5)
 
 user_last_request_time = {}
 
+# plan to use this script in your own bot? remove the disclaimer. it is useless outside @tg_dlpbot.
 menu_text = '''
 Привет! Отправь мне ссылку на видео, которое ты хочешь скачать. Максимальное качество - 720p.
+
+Бот лучше всего работает с видео не более 10-ти минут - если ваше видео больше, скорее всего бот просто не сможет его вам отправить - из-за лимита в 50MB самого Telegram.
 
 [!!!] Дисклеймер:
 
@@ -35,26 +39,23 @@ def download_video(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
     current_time = time.time()
     
-    # timeout
-    if user_id in user_last_request_time and (current_time - user_last_request_time[user_id] < 90):
-        update.message.reply_text("Подожди 90 секунд перед следующим запросом")
+    # cooldown between user requests 
+    if user_id in user_last_request_time and (current_time - user_last_request_time[user_id] < 45): # cooldown time in secs
+        update.message.reply_text("Подожди 45 секунд перед следующим запросом")
         return
-
     user_last_request_time[user_id] = current_time
-    
+
+    # remove this to allow downloading of playlists/channels 
     url = update.message.text
     if 'youtube.com/playlist' in url or 'youtube.com/channel' in url:
         update.message.reply_text("Для сохранения пропускной способности сохранение целых каналов не поддерживается.")
         return
-
     ydl_opts = {
-        'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',
-        'outtmpl': 'downloads/%(title)s.%(ext)s',
-        'recode-video': 'mp4',
-        'sleep_interval': 3,  # rate limit for yt-dlp
-        'max_sleep_interval': 3
+        'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]', # quality [1080 - Full HD, 720 - 720p and so on.]
+        'outtmpl': 'downloads/%(title)s.%(ext)s', # refer to yt-dlp docs for naming
+        'sleep_interval': 3,  # beware of youtube's rate limits if you set it to zero
+        'max_sleep_interval': 3 # same
     }
-
     def download_task():
         try:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -65,14 +66,14 @@ def download_video(update: Update, context: CallbackContext) -> None:
                     update.message.reply_video(video=video_file)
                 os.remove(video_title)
         except Exception as e:
-            update.message.reply_text("Произошла ошибка")
+            update.message.reply_text("Произошла ошибка") # will always occur if requested video is too long
         finally:
             download_semaphore.release()
-
     download_semaphore.acquire()
     threading.Thread(target=download_task).start()
 
 def main() -> None:
+    # your bot api token
     TOKEN = 'YOUR_TELEGRAM_API_KEY_HERE'
     
     updater = Updater(TOKEN)
